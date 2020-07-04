f428ab64b41058b12e8c8c3d24d97e3b3e90b91e
William Cao <williamcao999@gmail.com>
2020-07-04 18:48:10 -0400

    use javafx beans

--- src/main/java/budgeting/balance/Balance.java
+++ src/main/java/budgeting/balance/Balance.java
@@ -1,15 +1,46 @@
 package budgeting.balance;
 
-public class Balance {
-    private final int credit;
-    private final int debit;
-    private final int cash;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public final class Balance {
+    private final IntegerProperty credit = new SimpleIntegerProperty();
+    private final IntegerProperty debit = new SimpleIntegerProperty();
+    private final IntegerProperty cash = new SimpleIntegerProperty();
 
     public Balance(int credit, int debit, int cash){
-        this.credit = credit;
-        this.debit = debit;
-        this.cash = cash;
+        this.credit.set(credit);
+        this.debit.set(debit);
+        this.cash.set(cash);
     }
 
+    public void setCredit(int value){
+        this.credit.set(value);
+    }
+    public int getCredit(){
+        return this.credit.get();
+    }
+    public IntegerProperty creditProperty(){
+        return this.credit;
+    }
+
+    public void setDebit(int value){
+        this.debit.set(value);
+    }
+    public int getDebit(){
+        return this.debit.get();
+    }
+    public IntegerProperty debitProperty(){
+        return this.debit;
+    }
     
+    public void setCash(int value){
+        this.cash.set(value);
+    }
+    public int getCash(){
+        return this.cash.get();
+    }
+    public IntegerProperty cashProperty(){
+        return this.cash;
+    }
 }
